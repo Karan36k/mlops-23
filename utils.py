b0e822d1299b5ff226ffcab0d13eac286c29b434
Karan Arora <51876732+Karan36k@users.noreply.github.com>
2023-09-01 23:48:53 +0530

    Create utils.py
    Added split_train_dev_test and predict_and_eval functions

---
+++ utils.py
@@ -0,0 +1,9 @@
+# utils.py
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+def split_train_dev_test(X, y, test_size, dev_size):
+    # Implementation of split_train_dev_test function
+
+def predict_and_eval(model, X_test, y_test):
+    # Implementation of predict_and_eval function
